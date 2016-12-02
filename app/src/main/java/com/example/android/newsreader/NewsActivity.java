281d331cf02ccd78d675854c8e4c079876470b5b
Smriti Das <dassmrit@gmail.com>
2016-12-01 19:22:16 -0500

    Updated the manifest and layout.

--- app/src/main/java/com/example/android/newsreader/NewsActivity.java
+++ app/src/main/java/com/example/android/newsreader/NewsActivity.java
@@ -3,11 +3,14 @@ package com.example.android.newsreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity {
+
+    public final static LOG_TAG = NewsActivity.class.getName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.news_main);
     }
 }
