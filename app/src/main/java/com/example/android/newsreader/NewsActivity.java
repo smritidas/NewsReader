c63481d61661a9adc72615d43c2644ad2f371277
Smriti Das <dassmrit@gmail.com>
2016-12-02 08:35:15 -0500

    Updated the manifest and layout.

--- app/src/main/java/com/example/android/newsreader/NewsActivity.java
+++ app/src/main/java/com/example/android/newsreader/NewsActivity.java
@@ -2,15 +2,38 @@ package com.example.android.newsreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
 
-    public final static LOG_TAG = NewsActivity.class.getName();
-
+    public static final String LOG_TAG = NewsActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_main);
+
+        //Creating a fake list of news items
+        ArrayList<String> news = new ArrayList<>();
+        news.add("Main");
+        news.add("Entertainment");
+        news.add("Politics");
+        news.add("Sports");
+        news.add("World");
+        news.add("Business");
+
+        //Find a reference to the listview in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        //Create a new list of news items
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, news
+        );
+
+        //Set the adapter so the list can be populated in the user interface
+        newsListView.setAdapter(adapter);
     }
 }
